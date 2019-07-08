5911a798495285a0a8a472d5fd2c4781153267de
Dhanesh Gosai <dgosai@cisco.com>
2019-07-08 12:10:36 +0530

    New Project Added

---
+++ NSLayoutAnchorDemo/NSLayoutAnchorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NSLayoutAnchorDemo
+//
+//  Created by Sheshnath on 08/07/19.
+//  Copyright © 2019 Exp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NSLayoutAnchorDemo/NSLayoutAnchorDemoTests/NSLayoutAnchorDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  NSLayoutAnchorDemoTests.swift
+//  NSLayoutAnchorDemoTests
+//
+//  Created by Sheshnath on 08/07/19.
+//  Copyright © 2019 Exp. All rights reserved.
+//
+
+import XCTest
+@testable import NSLayoutAnchorDemo
+
+class NSLayoutAnchorDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
